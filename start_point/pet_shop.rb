63a16c2501c11046568a2f78d334e1f92bebeaf7
Stephen Ramsay <rabrandom@gmail.com>
2019-11-22 14:28:24 +0000

    complete 2 activites

--- start_point/pet_shop.rb
+++ start_point/pet_shop.rb
@@ -1,3 +1,9 @@
-def pet_shop_name(name)
-  return name @petshop[:name]
+# 1
+def pet_shop_name(pet_shop)
+ return pet_shop[:name]
+end
+
+# 2
+def total_cash(pet_shop)
+  return pet_shop[:admin][:total_cash]
 end
