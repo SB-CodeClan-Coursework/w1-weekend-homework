a566956e067fe8fbe5861c50ed5913475a675e34
Stephen Ramsay <rabrandom@gmail.com>
2019-11-22 15:23:43 +0000

    complete 7 questions

--- start_point/pet_shop.rb
+++ start_point/pet_shop.rb
@@ -10,5 +10,27 @@ end
 
 # 3
 def add_or_remove_cash(pet_shop, add_number)
-  return pet_shop[:admin][:total_cash] += 10
+  return pet_shop[:admin][:total_cash] += add_number
 end
+
+# 4
+def add_or_remove_cash(pet_shop, subtract_number)
+  return pet_shop[:admin][:total_cash] += subtract_number
+end
+
+# 5
+def pets_sold(pet_shop)
+  return pet_shop[:admin][:pets_sold]
+end
+
+# 6
+def increase_pets_sold(pet_shop, add_two)
+  return pet_shop[:admin][:pets_sold] += add_two
+end
+
+# 7
+def stock_count(pet_shop)
+  return pet_shop[:pets].count
+end
+
+#  8
